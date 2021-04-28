95e8b4f684767b4815a10057b6a29d04d94b6ab8
Erick Ferreira <erick.xferreira@gmail.com>
2021-04-27 21:19:31 -0300

    style: clicked filter

--- src/components/TaskFooter/index.js
+++ src/components/TaskFooter/index.js
@@ -6,20 +6,26 @@ const TaskFooter = () => {
 
     const {tasks, active, all, completed, clear} = React.useContext(GlobalContext)
     const left = tasks.filter( item => item.isActive ).length
+    const [clicked, setClicked] = React.useState(null)
 
     return (
         <div className='todos--footer'>
             <span>{left} items left</span>
             <span>
-                <button onClick={() => {all()}}>All</button>
-                <button onClick={() => {active()}}>Active</button>
-                <button onClick={() => {completed()}}>Completed</button>
+                <button 
+                    className={clicked  === 'all' ? clicked : null}
+                    onClick={() => {all(); setClicked('all')}}>All</button>
+                <button 
+                    className={clicked  === 'active' ? clicked : null}
+                    onClick={() => {active(); setClicked('active')}}>Active</button>
+                <button 
+                    className={clicked  === 'completed' ? clicked : null}
+                    onClick={() => {completed(); setClicked('completed')}}>Completed</button>
             </span>
 
             <span><button onClick={() => {clear()}} className='todos--footer-clear'>Clear Completed</button></span>
         </div>
     )
 }
-
 
 export default TaskFooter;
